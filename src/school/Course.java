a22957f48eb6a7c17ec213f93170bba697dd5561
Melissa Pabst <melissapabst@gmail.com>
2018-05-22 08:11:57 -0500

    begin restaurant menu build

--- src/school/Course.java
+++ src/school/Course.java
@@ -1,4 +1,17 @@
 package school;
 
 public class Course {
+
+    String name;
+    String description;
+    double roomNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+
 }
